6d4d5dc105d4855b7622aacf183e8d260e543f70
Valentin Zickner <valentin.zickner@mimacom.com>
2016-09-25 18:17:10 +0200

    Add possibility to deploy over ssh to dev server.

---
+++ client/src/main/java/de/helfekannjeder/helfomat/client/ClientApplication.java
@@ -0,0 +1,15 @@
+package de.helfekannjeder.helfomat.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Valentin Zickner
+ */
+@SpringBootApplication
+public class ClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
