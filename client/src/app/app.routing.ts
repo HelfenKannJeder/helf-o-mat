f015d2d98fbe7c2f3e439dff1bacbf6838600d98
Valentin Zickner <valentin.zickner@helfenkannjeder.de>
2017-09-20 08:27:51 +0200

    Minor design and behaviour fixes for questions page.

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,18 +1,16 @@
-import {ModuleWithProviders} from "@angular/core";
-import {Routes, RouterModule} from "@angular/router";
-import {QuestionComponent} from "./question/question.component";
-import {ResultComponent} from "./result/result.component";
-import {OrganisationComponent} from "./organisation/organisation.component";
+import {ModuleWithProviders} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {QuestionComponent} from './question/question.component';
+import {ResultComponent} from './result/result.component';
+import {OrganisationComponent} from './organisation/organisation.component';
 
 const appRoutes: Routes = [
-    { path: 'result', component: ResultComponent },
-    { path: 'organisation', component: OrganisationComponent },
-    { path: 'question', component: QuestionComponent },
-    { path: '', component: QuestionComponent }
+    {path: 'result', component: ResultComponent},
+    {path: 'organisation', component: OrganisationComponent},
+    {path: 'question', component: QuestionComponent},
+    {path: '', redirectTo: '/question', pathMatch: 'full'}
 ];
 
-export const appRoutingProviders: any[] = [
-
-];
+export const appRoutingProviders: any[] = [];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
