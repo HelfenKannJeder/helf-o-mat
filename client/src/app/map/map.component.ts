73c58e770e11dc49a60bbab11272019753daa4d4
Valentin Zickner <valentin.zickner@helfenkannjeder.de>
2017-09-23 11:34:45 +0200

    Optimize map component.

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Organisation} from '../organisation/organisation.model';
 import {Observable} from 'rxjs';
 import {GeoPoint} from '../organisation/geopoint.model';
@@ -27,6 +27,7 @@ export class MapComponent implements OnInit, AfterViewInit {
     @Input() distance: Observable<number>;
     @Input() zoom: Observable<number>;
     @Input() clusteredOrganisations: Observable<GeoPoint[]>;
+    @Input() allowUpdatePosition: boolean = true;
 
     @Output() updatePosition: EventEmitter<GeoPoint> = new EventEmitter<GeoPoint>();
     @Output() updateBoundingBox: EventEmitter<BoundingBox> = new EventEmitter<BoundingBox>();
@@ -40,11 +41,15 @@ export class MapComponent implements OnInit, AfterViewInit {
     private positionCircle: Circle;
     private markerClusterer: MarkerClusterer;
 
-    constructor() {
+    constructor(private element: ElementRef) {
     }
 
     ngOnInit() {
-        let mapProp = {
+
+    }
+
+    ngAfterViewInit() {
+        this.map = new Map(this.element.nativeElement.getElementsByClassName('googleMap')[0], {
             mapTypeId: MapTypeId.ROADMAP,
             zoomControl: true,
             mapTypeControl: false,
@@ -52,30 +57,33 @@ export class MapComponent implements OnInit, AfterViewInit {
             streetViewControl: false,
             rotateControl: false,
             fullscreenControl: true
-        };
-        this.map = new Map(document.getElementById('googleMap'), mapProp);
-
-        // Create the search box and link it to the UI element.
-        let searchText: HTMLInputElement = <HTMLInputElement>document.getElementById('pac-input');
-        let searchBox = new SearchBox(searchText);
-        this.map.controls[ControlPosition.TOP_LEFT].push(searchText);
-
-        searchBox.addListener('places_changed', () => {
-            let places = searchBox.getPlaces();
-
-            if (places.length == 0) {
-                return;
-            }
-
-            this.updatePosition.next(MapComponent.convertLatLngToGeoPoint(places[0].geometry.location));
         });
 
+        this.configureViewPortChange();
+        this.configureDrawUserPosition();
+        this.configureUpdateViewPort();
+
+        if (this.canChoosePosition()) {
+            this.configureSearchBox();
+        }
+        if (this.hasOrganisations()) {
+            this.configureDrawOrganisations();
+        }
+
+        this.markerClusterer = new MarkerClusterer(this.map, [], {imagePath: 'assets/images/m'});
+        if (this.hasClustedOrganisations()) {
+            this.drawClusteredOrganisations();
+        }
+
+        google.maps.event.trigger(this.map, 'resize');
+    }
+
+    private configureViewPortChange() {
         this.map.addListener('bounds_changed', () => {
             let bounds = this.map.getBounds();
             if (bounds == null) {
                 return;
             }
-            searchBox.setBounds(bounds);
 
             const northEast = MapComponent.convertLatLngToGeoPoint(bounds.getNorthEast());
             const southWest = MapComponent.convertLatLngToGeoPoint(bounds.getSouthWest());
@@ -86,18 +94,41 @@ export class MapComponent implements OnInit, AfterViewInit {
         this.map.addListener('zoom_changed', () => {
             this.updateZoom.emit(this.map.getZoom());
         });
+    }
 
+    private configureDrawUserPosition() {
         Observable.combineLatest(
             this.position,
             this.distance
         ).subscribe((newSearchRange: [GeoPoint, number]) => {
             const mapsPosition = MapComponent.convertGeoPointToLatLng(newSearchRange[0]);
-            if (this.map.getBounds() == undefined
-                || !this.map.getBounds().contains(mapsPosition)) {
-            }
             this.drawUserPosition(mapsPosition, newSearchRange[1]);
         });
+    }
 
+    private configureSearchBox() {
+        let searchText: HTMLInputElement = this.element.nativeElement.getElementsByClassName('addressInput')[0];
+        let searchBox = new SearchBox(searchText);
+        this.map.controls[ControlPosition.TOP_LEFT].push(searchText);
+
+        searchBox.addListener('places_changed', () => {
+            let places = searchBox.getPlaces();
+
+            if (places.length != 0) {
+                this.updatePosition.next(MapComponent.convertLatLngToGeoPoint(places[0].geometry.location));
+            }
+        });
+
+        this.map.addListener('bounds_changed', () => {
+            let bounds = this.map.getBounds();
+
+            if (bounds != null) {
+                searchBox.setBounds(bounds);
+            }
+        });
+    }
+
+    private configureUpdateViewPort() {
         this.center.subscribe((center: GeoPoint) => {
             this.map.setCenter(MapComponent.convertGeoPointToLatLng(center));
         });
@@ -105,70 +136,72 @@ export class MapComponent implements OnInit, AfterViewInit {
         this.zoom.subscribe((zoom: number) => {
             this.map.setZoom(zoom);
         });
-
-        if (this.organisations !== undefined) {
-            this.organisations.subscribe((organisations) => {
-                this.markers.forEach((marker: Marker) => {
-                    marker.setMap(null);
-                });
-                this.markers = [];
-
-                organisations.forEach((organisation: Organisation) => {
-                    if (organisation.addresses.length > 0 && organisation.mapPin !== undefined) {
-                        for (let address of organisation.addresses) {
-                            const icon = {
-                                url: `assets/images/pins/${organisation.mapPin}`,
-                                size: new Size(32, 32),
-                                origin: new Point(0, 0),
-                                anchor: new Point(10, 32),
-                                scaledSize: new Size(32, 32)
-                            };
-
-                            let marker = new Marker({
-                                position: MapComponent.convertGeoPointToLatLng(address.location),
-                                map: this.map,
-                                title: organisation.name,
-                                icon: icon,
-                                opacity: organisation.scoreNorm / 100
-                            });
-                            marker.addListener('click', () => {
-                                this.openOrganisation.emit(organisation);
-                            });
-                            this.markers.push(marker);
-                        }
-                    }
-                });
-            });
-        }
-
-        if (this.clusteredOrganisations !== undefined) {
-            this.clusteredOrganisations.subscribe((clusteredOrganisations) => {
-                const icon = {
-                    url: 'assets/images/pins/gray.png',
-                    size: new Size(32, 32),
-                    origin: new Point(0, 0),
-                    anchor: new Point(10, 32),
-                    scaledSize: new Size(32, 32)
-                };
-
-                let newMarkers = clusteredOrganisations.map((geoPoint: GeoPoint) => {
-                    return new Marker({
-                        position: MapComponent.convertGeoPointToLatLng(geoPoint),
-                        icon
-                    });
-
-                });
-
-                this.clusteredMarkers = this.replaceMarkers(this.clusteredMarkers, newMarkers);
-
-            });
-        }
-
-        this.markerClusterer = new MarkerClusterer(this.map, [], {imagePath: 'assets/images/m'});
     }
 
-    ngAfterViewInit() {
-        google.maps.event.trigger(this.map, 'resize');
+    private hasOrganisations() {
+        return this.organisations !== undefined;
+    }
+
+    private configureDrawOrganisations() {
+        this.organisations.subscribe((organisations) => {
+            this.markers.forEach((marker: Marker) => {
+                marker.setMap(null);
+            });
+            this.markers = [];
+
+            organisations.forEach((organisation: Organisation) => {
+                if (organisation.addresses.length > 0 && organisation.mapPin !== undefined) {
+                    for (let address of organisation.addresses) {
+                        const icon = {
+                            url: `assets/images/pins/${organisation.mapPin}`,
+                            size: new Size(32, 32),
+                            origin: new Point(0, 0),
+                            anchor: new Point(10, 32),
+                            scaledSize: new Size(32, 32)
+                        };
+
+                        let marker = new Marker({
+                            position: MapComponent.convertGeoPointToLatLng(address.location),
+                            map: this.map,
+                            title: organisation.name,
+                            icon: icon,
+                            opacity: organisation.scoreNorm / 100
+                        });
+                        marker.addListener('click', () => {
+                            this.openOrganisation.emit(organisation);
+                        });
+                        this.markers.push(marker);
+                    }
+                }
+            });
+        });
+    }
+
+    private hasClustedOrganisations() {
+        return this.clusteredOrganisations !== undefined;
+    }
+
+    private drawClusteredOrganisations() {
+        this.clusteredOrganisations.subscribe((clusteredOrganisations) => {
+            const icon = {
+                url: 'assets/images/pins/gray.png',
+                size: new Size(32, 32),
+                origin: new Point(0, 0),
+                anchor: new Point(10, 32),
+                scaledSize: new Size(32, 32)
+            };
+
+            let newMarkers = clusteredOrganisations.map((geoPoint: GeoPoint) => {
+                return new Marker({
+                    position: MapComponent.convertGeoPointToLatLng(geoPoint),
+                    icon
+                });
+
+            });
+
+            this.clusteredMarkers = this.replaceMarkers(this.clusteredMarkers, newMarkers);
+
+        });
     }
 
     drawUserPosition(position: LatLng, distance: number) {
@@ -181,12 +214,14 @@ export class MapComponent implements OnInit, AfterViewInit {
         this.positionMarker = new Marker({
             map: this.map,
             position: position,
-            draggable: true
+            draggable: this.canChoosePosition()
         });
 
-        google.maps.event.addListener(this.positionMarker, 'drag', (event) => {
-            this.updatePosition.next(MapComponent.convertLatLngToGeoPoint(event.latLng));
-        });
+        if (this.canChoosePosition()) {
+            google.maps.event.addListener(this.positionMarker, 'drag', (event) => {
+                this.updatePosition.next(MapComponent.convertLatLngToGeoPoint(event.latLng));
+            });
+        }
 
         // Add circle overlay and bind to marker
         this.positionCircle = new Circle({
@@ -197,6 +232,10 @@ export class MapComponent implements OnInit, AfterViewInit {
             fillColor: '#000000'
         });
         this.positionCircle.bindTo('center', this.positionMarker, 'position');
+    }
+
+    canChoosePosition(): boolean {
+        return this.allowUpdatePosition;
     }
 
     private static convertGeoPointToLatLng(location: GeoPoint): LatLng {
