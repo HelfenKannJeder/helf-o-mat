11f4619330ce57d6ee8f0cfacb9dc742b6bf3226
Valentin Zickner <valentin.zickner@helfenkannjeder.de>
2017-09-24 21:05:30 +0200

    Adapt design from result side.

--- client/src/app/organisation/organisation.module.ts
+++ client/src/app/organisation/organisation.module.ts
@@ -1,15 +1,15 @@
 import {NgModule} from '@angular/core';
 import {OrganisationComponent} from './organisation.component';
-import {MapModule} from '../map/map.module';
 import {AnswerImageModule} from '../shared/answer-image.module';
 import {BrowserModule} from '@angular/platform-browser';
 import {OrganisationScoreModule} from '../_internal/components/organisation-score.module';
 import {TranslateModule} from '@ngx-translate/core';
+import {AlternativeMapModule} from '../map/alternative/alternative-map.module';
 
 @NgModule({
     imports: [
         TranslateModule,
-        MapModule,
+        AlternativeMapModule,
         AnswerImageModule,
         BrowserModule,
         OrganisationScoreModule
