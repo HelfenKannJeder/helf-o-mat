122498b6f7de4b64e68dc77c75239ab0da6c3463
Valentin Zickner <valentin.zickner@helfenkannjeder.de>
2017-11-17 23:28:36 +0100

    Adapt Elasticsearch Configuration to fix conversion of LocalTime.

---
+++ helfomat-infrastructure-elasticsearch/src/main/java/de/helfenkannjeder/helfomat/infrastructure/elasticsearch/ElasticsearchConfig.java
@@ -0,0 +1,47 @@
+package de.helfenkannjeder.helfomat.infrastructure.elasticsearch;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.elasticsearch.client.Client;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.EntityMapper;
+
+import java.io.IOException;
+
+@Configuration
+public class ElasticsearchConfig {
+
+    @Bean
+    public ElasticsearchTemplate elasticsearchTemplate(Client client) {
+        return new ElasticsearchTemplate(client, new CustomEntityMapper());
+    }
+
+    public static class CustomEntityMapper implements EntityMapper {
+
+        private final ObjectMapper objectMapper;
+
+        CustomEntityMapper() {
+            objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+            objectMapper.registerModule(new JavaTimeModule());
+            objectMapper.registerModule(new Jdk8Module());
+        }
+
+        @Override
+        public String mapToString(Object object) throws IOException {
+            return objectMapper.writeValueAsString(object);
+        }
+
+        @Override
+        public <T> T mapToObject(String source, Class<T> clazz) throws IOException {
+            return objectMapper.readValue(source, clazz);
+        }
+
+    }
+
+}
