a2df3395f2d8467f207466ff79d2e87b9efc6fe1
Valentin Zickner <valentin.zickner@helfenkannjeder.de>
2017-12-03 15:35:05 +0100

    Fix reader to skip null items and continue importing.

--- helfomat-infrastructure-typo3/src/main/java/de/helfenkannjeder/helfomat/infrastructure/typo3/Typo3OrganisationReader.java
+++ helfomat-infrastructure-typo3/src/main/java/de/helfenkannjeder/helfomat/infrastructure/typo3/Typo3OrganisationReader.java
@@ -3,7 +3,8 @@ package de.helfenkannjeder.helfomat.infrastructure.typo3;
 import de.helfenkannjeder.helfomat.core.ProfileRegistry;
 import de.helfenkannjeder.helfomat.core.organisation.Organisation;
 import de.helfenkannjeder.helfomat.core.organisation.OrganisationReader;
-import org.springframework.batch.item.database.AbstractPagingItemReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
@@ -12,7 +13,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 /**
@@ -22,36 +25,57 @@ import java.util.stream.Collectors;
 @Order(100)
 @Transactional(propagation = Propagation.REQUIRED)
 @Profile("!" + ProfileRegistry.DISABLE_TYPO3_IMPORT)
-public class Typo3OrganisationReader extends AbstractPagingItemReader<Organisation> implements OrganisationReader {
+public class Typo3OrganisationReader implements OrganisationReader {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Typo3OrganisationReader.class);
+    private static final int PAGE_SIZE = 10;
+
+    private volatile List<Organisation> results = null;
+    private volatile int page = 0;
+    private volatile int current = 0;
+
 
     private Typo3OrganisationRepository typo3OrganisationRepository;
     private Typo3OrganisationProcessor typo3OrganisationProcessor;
+
 
     @Autowired
     public Typo3OrganisationReader(Typo3OrganisationRepository typo3OrganisationRepository, Typo3OrganisationProcessor typo3OrganisationProcessor) {
         this.typo3OrganisationRepository = typo3OrganisationRepository;
         this.typo3OrganisationProcessor = typo3OrganisationProcessor;
-        this.setName(this.getClass().getSimpleName());
     }
 
-    @Override
-    protected void doReadPage() {
+    private void doReadPage() {
         if (this.results == null) {
-            this.results = new ArrayList<>();
+            this.results = new CopyOnWriteArrayList<>();
         } else {
             this.results.clear();
         }
 
         this.results.addAll(
             this.typo3OrganisationRepository
-                .findAvailable(new PageRequest(getPage(), getPageSize()))
-            .map(organisation -> this.typo3OrganisationProcessor.process(organisation))
-            .collect(Collectors.toList())
+                .findAvailable(new PageRequest(page, PAGE_SIZE))
+                .map(organisation -> this.typo3OrganisationProcessor.process(organisation))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList())
         );
     }
 
     @Override
-    protected void doJumpToPage(int i) {
+    public Organisation read() throws Exception {
+        if (results == null || current >= results.size()) {
+            LOGGER.debug("Reading page " + page);
+            doReadPage();
+            page++;
+            current = 0;
+        }
+
+        int next = current++;
+        if (next < results.size()) {
+            return results.get(next);
+        } else {
+            return null;
+        }
     }
 
 }
