588e0208fe5f0801b6e9ff5f03f8e685576b8304
Valentin Zickner <valentin.zickner@helfenkannjeder.de>
2020-04-19 13:07:22 -0400

    add docker images for import and web-ui

--- travis/helfomat-web_dockerBuildAndPublish.sh
+++ travis/helfomat-web_dockerBuildAndPublish.sh
@@ -1,4 +1,10 @@
+set -xe
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-echo "build and push image: $PROJECT_VERSION-${TRAVIS_COMMIT:0:8}"
-./mvnw package com.google.cloud.tools:jib-maven-plugin:build -DskipTests -D image=helfenkannjeder/helfomat-web:$PROJECT_VERSION-${TRAVIS_COMMIT:0:8} -pl helfomat-web -am
+BUILD_TAG=$PROJECT_VERSION-${TRAVIS_COMMIT:0:8}
+echo "build and push image: $BUILD_TAG"
+./mvnw package com.google.cloud.tools:jib-maven-plugin:build -Phelfomat-web -DskipTests -D image=helfenkannjeder/helfomat-web:$BUILD_TAG -pl helfomat-web -am
+./mvnw package com.google.cloud.tools:jib-maven-plugin:build -Phelfomat-import -DskipTests -D image=helfenkannjeder/helfomat-import:$BUILD_TAG -pl helfomat-import -am
+./mvnw package -Phelfomat-web-ui,docker -DskipTests
+docker build -t helfenkannjeder/helfomat-web-ui:$BUILD_TAG helfomat-web-ui/docker/
+docker push helfenkannjeder/helfomat-web-ui:$BUILD_TAG
