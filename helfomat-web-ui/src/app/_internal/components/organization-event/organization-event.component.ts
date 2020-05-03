d17bf056c7343285c461f41af5a720632531ceef
Valentin Zickner <valentin.zickner@helfenkannjeder.de>
2020-05-02 20:33:16 -0400

    improve visualization of changed group edit organization events

--- helfomat-web-ui/src/app/_internal/components/organization-event/organization-event.component.ts
+++ helfomat-web-ui/src/app/_internal/components/organization-event/organization-event.component.ts
@@ -16,4 +16,16 @@ export class OrganizationEventComponent {
     @Input()
     public organizationEvents: Array<OrganizationEvent> = [];
 
+    public isDeleteEvent(eventType: string): boolean {
+        return eventType.indexOf("Delete") >= 0;
+    }
+
+    public isCreateEvent(eventType: string): boolean {
+        return eventType.indexOf("Add") >= 0;
+    }
+
+    public isChange(eventType: string): boolean {
+        return !this.isDeleteEvent(eventType) && !this.isCreateEvent(eventType);
+    }
+
 }
