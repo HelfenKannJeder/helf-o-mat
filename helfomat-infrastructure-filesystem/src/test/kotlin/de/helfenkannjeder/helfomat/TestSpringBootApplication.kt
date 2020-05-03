c8a693be2bb1430a35030fdf93365af1eb672711
Valentin Zickner <valentin.zickner@helfenkannjeder.de>
2020-05-02 20:42:33 -0400

    fix tests

--- helfomat-infrastructure-filesystem/src/test/kotlin/de/helfenkannjeder/helfomat/TestSpringBootApplication.kt
+++ helfomat-infrastructure-filesystem/src/test/kotlin/de/helfenkannjeder/helfomat/TestSpringBootApplication.kt
@@ -1,16 +1,19 @@
 package de.helfenkannjeder.helfomat
 
 import de.helfenkannjeder.helfomat.api.geopoint.DistanceMatrixApplicationService
+import de.helfenkannjeder.helfomat.core.approval.ApprovalRepository
 import de.helfenkannjeder.helfomat.core.organization.OrganizationRepository
 import de.helfenkannjeder.helfomat.core.question.QuestionRepository
 import de.helfenkannjeder.helfomat.core.template.OrganizationTemplateRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 /**
  * @author Valentin Zickner
  */
 @SpringBootApplication
+@EnableJpaRepositories
 open class TestSpringBootApplication {
     
     @MockBean
@@ -24,5 +27,8 @@ open class TestSpringBootApplication {
 
     @MockBean
     private lateinit var distanceMatrixApplicationService: DistanceMatrixApplicationService
+
+    @MockBean
+    lateinit var approvalRepository: ApprovalRepository
 
 }
