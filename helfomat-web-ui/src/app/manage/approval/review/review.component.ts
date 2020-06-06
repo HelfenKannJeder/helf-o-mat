e77f8e6ae33b233f41c2d504d16aec2ccc2f7047
Valentin Zickner <valentin.zickner@helfenkannjeder.de>
2020-06-06 14:09:42 -0400

    ensure approval duplicates are not published (resolves #188)

--- helfomat-web-ui/src/app/manage/approval/review/review.component.ts
+++ helfomat-web-ui/src/app/manage/approval/review/review.component.ts
@@ -1,9 +1,9 @@
 import {Component} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
-import {flatMap} from "rxjs/operators";
+import {distinctUntilChanged, filter, flatMap, map, mergeMap} from "rxjs/operators";
 import {ApprovalDetailDto, ApprovalId, ApprovalService} from "../../../_internal/resources/approval.service";
 import {ObservableUtil} from "../../../shared/observable.util";
-import {Subject} from "rxjs";
+import {BehaviorSubject, of, Subject} from "rxjs";
 import {TranslateService} from "@ngx-translate/core";
 import {ToastrService} from "ngx-toastr";
 
@@ -14,6 +14,7 @@ import {ToastrService} from "ngx-toastr";
 export class ReviewComponent {
 
     public approval: Subject<ApprovalDetailDto> = new Subject<ApprovalDetailDto>();
+    private doApprove: Subject<ApprovalId> = new BehaviorSubject(null);
 
     constructor(
         private route: ActivatedRoute,
@@ -32,19 +33,26 @@ export class ReviewComponent {
             .subscribe(approval => {
                 this.approval.next(approval);
             })
-    }
-
-    approve(approvalId: ApprovalId) {
-        this.approvalService.confirmApproval(approvalId)
+        this.doApprove
             .pipe(
-                flatMap(() => {
-                    return this.approvalService.findAll()
-                })
+                distinctUntilChanged((id1, id2) => id1?.value === id2?.value),
+                mergeMap((approvalId: ApprovalId) => {
+                    if (approvalId == null) {
+                        return of(null);
+                    }
+                    return this.approvalService.confirmApproval(approvalId)
+                        .pipe(map(() => approvalId));
+                }),
+                filter(e => e != null)
             )
             .subscribe(() => {
                 const message = this.translateService.instant('manage.organization.approval.success');
                 this.toastr.success(message);
                 this.router.navigate(["/admin/approval"])
             });
+    }
+
+    approve(approvalId: ApprovalId) {
+        this.doApprove.next(approvalId);
     }
 }
