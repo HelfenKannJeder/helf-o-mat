460a6043f345d112d33901e7dcd085f35d1b18f5
Valentin Zickner <valentin.zickner@helfenkannjeder.de>
2020-08-29 14:40:03 +0200

    remove legacy import

--- helfomat-core/src/main/kotlin/de/helfenkannjeder/helfomat/core/ProfileRegistry.kt
+++ helfomat-core/src/main/kotlin/de/helfenkannjeder/helfomat/core/ProfileRegistry.kt
@@ -7,7 +7,6 @@ object ProfileRegistry {
     const val MOCK_DOWNLOAD = "mock-download"
     const val MOCK_DISTANCE_CALCULATION = "mock-distance-calculation"
     const val DISABLE_THWDE_IMPORT = "disable-thwde-import"
-    const val DISABLE_TYPO3_IMPORT = "disable-typo3-import"
     const val ENABLE_EMAIL = "enable-email"
     const val TEST = "test"
     const val AZURE = "azure"
