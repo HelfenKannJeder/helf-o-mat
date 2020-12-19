ac949d32d3b4eaf130d7a33ffa7478590c0c37c3
Valentin Zickner <3200232+vzickner@users.noreply.github.com>
2020-12-19 18:39:53 +0100

    upgrade spring boot to 2.4.1 (#219)

--- helfomat-core/src/test/kotlin/de/helfenkannjeder/helfomat/core/geopoint/GeoPointTest.kt
+++ helfomat-core/src/test/kotlin/de/helfenkannjeder/helfomat/core/geopoint/GeoPointTest.kt
@@ -1,7 +1,7 @@
 package de.helfenkannjeder.helfomat.core.geopoint
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 class GeoPointTest {
 
