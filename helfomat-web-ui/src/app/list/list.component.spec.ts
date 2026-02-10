3a0811add745548488b372696d6779abaa2f0906
KrugT <KrugT@users.noreply.github.com>
2026-02-10 20:59:06 +0100

    tests: fix for new service used in component

--- helfomat-web-ui/src/app/list/list.component.spec.ts
+++ helfomat-web-ui/src/app/list/list.component.spec.ts
@@ -4,7 +4,7 @@ import {ListComponent} from './list.component';
 
 describe('Component: List', () => {
     it('should create an instance', () => {
-        let component = new ListComponent(null);
+        let component = new ListComponent(null, null);
         expect(component).toBeTruthy();
     });
 });
